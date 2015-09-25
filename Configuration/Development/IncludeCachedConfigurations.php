5ecff10cdfd04661ed93a22f0f6218977b3cf8a2
Flo <alphalogic@gmail.com>
2015-09-25 10:44:39 +0200

    Added example configuration

--- Configuration/Development/IncludeCachedConfigurations.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (FLOW_PATH_ROOT !== '/home/flo/Dokumente/www/hostings/hausnetz/' || !file_exists('/home/flo/Dokumente/www/hostings/hausnetz/Data/Temporary/Development/Configuration/DevelopmentConfigurations.php')) {
-	unlink(__FILE__);
-	return array();
-}
-return require '/home/flo/Dokumente/www/hostings/hausnetz/Data/Temporary/Development/Configuration/DevelopmentConfigurations.php';
