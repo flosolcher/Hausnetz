54868e4e2d6980d6ae7269ff257f926adaa793cf
Florian Konnertz <f_konnertz@syscomp.de>
2015-09-29 12:01:32 +0200

    edit,new okay uri arguments added, flash messages ok

--- Packages/Application/Grandhotel.Hausnetz/Classes/Grandhotel/Hausnetz/Controller/NoteController.php
+++ Packages/Application/Grandhotel.Hausnetz/Classes/Grandhotel/Hausnetz/Controller/NoteController.php
@@ -8,7 +8,7 @@ namespace Grandhotel\Hausnetz\Controller;
 
 use Grandhotel\Hausnetz\Controller\Super\AbstractController;
 use TYPO3\Flow\Annotations as Flow;
-use Grandhotel\Hausnetz\Domain\Model\Note;
+use Grandhotel\Hausnetz\Domain\Model\Note as Note;
 
 
 class NoteController extends AbstractController {
@@ -20,7 +20,6 @@ class NoteController extends AbstractController {
     protected $noteRepository;
     
     /**
-     * @return void
      */
     public function indexAction() {
       $user = $this->authService->getCurrentUser();
@@ -30,48 +29,50 @@ class NoteController extends AbstractController {
     }
     
     /**
-     * @return void
      */
     public function newAction() {
       $this->view->assign('action', 'create');
     }
 
-    /*
-     * @param \Grandhotel\Hausnetz\Domain\Model\Note $note
-     * @return void
+    /**
+     * @param Note $note
      */
     public function editAction(Note $note) {
       $this->view->assign('note', $note);
       $this->view->assign('action', 'update');
     }
 
-    /*
-     * @param \Grandhotel\Hausnetz\Domain\Model\Note $note
-     * @return void 
+    /**
+     * @param Note $note
      */
     public function deleteAction(Note $note) {
-        $this->noteRepository->delete($note);
+        $title = $note->getTitle();
+        $this->noteRepository->remove($note);
+        $this->persistenceManager->persistAll();
+        $this->addFlashMessage("Die Notiz $title wurde gelöscht.");
         $this->redirect('index');
     }
 
     
     /**
     * @param Note $note
-    * return void
     */
     public function createAction(Note $note) {
+        $title = $note->getTitle();
         $note->setActive = TRUE;
         $user = $this->authService->getCurrentUser();
         $note->setUser($user);
         $this->noteRepository->add($note);
+        $this->addFlashMessage("Die neue Notiz $title wurde gespeichert.");
         $this->redirect('index');
     }    
     /**
     * @param Note $note
-    * return void
     */
     public function updateAction(Note $note) {
+        $title = $note->getTitle();
         $this->noteRepository->update($note);
+        $this->addFlashMessage("Die Änderungen an Notiz $title wurden gespeichert.");
         $this->redirect('index');
     }
 }
