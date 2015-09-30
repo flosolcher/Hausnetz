1be92bfdbf6d0f9aaa80911c5750006539042684
Florian Konnertz <fkonnertz@posteo.de>
2015-09-30 12:20:49 +0200

    fields of Contact not yet implemented

---
+++ contact_todo.php
@@ -0,0 +1,54 @@
+
+function away() {
+    
+    class blub {
+    
+    /**
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * @var string
+     */
+    protected $fax;
+
+    /**
+     * @var string
+     */
+    protected $cellphone;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $website;
+
+    /**
+     * @var string
+     */
+    protected $facebook;
+
+    /**
+     * @var string
+     */
+    protected $twitter;
+    
+    /**
+     * @var string
+     */
+    protected $company;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    protected $memo;
+    
+    }
+}
+
